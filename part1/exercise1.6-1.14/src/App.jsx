49e8ec102113ed303762a773cb36e98de32d2462
Jnajera0430 <desarrolador.sa.12@gmail.com>
2024-03-21 13:07:24 -0500

    solucion exercise 1.7

--- part1/exercise1.6-1.14/src/App.jsx
+++ part1/exercise1.6-1.14/src/App.jsx
@@ -4,9 +4,9 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const Content = ({ text, total }) => (
+const Content = ({ text, total, caracter }) => (
   <p>
-    {text} {total}
+    {text} {total} {caracter}
   </p>
 );
 
@@ -14,15 +14,35 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [totalpro, setTotalpro] = useState(0);
+  const [percentagePositive, setPercentagePositive] = useState(0);
 
   const handleGoodClick = () => {
-    setGood(1 + good);
+    const updateTotal = total + 1;
+    const updateTotalPro = totalpro + 1;
+    const updateGood = good + 1;
+    setGood(updateGood);
+    setTotal(updateTotal);
+    setTotalpro(updateTotalPro);
+    setAverage(updateTotalPro / updateTotal);
+    setPercentagePositive((updateGood / updateTotal) * 100);
   };
   const handleNeutralClick = () => {
     setNeutral(1 + neutral);
+    const updateTotal = total + 1;
+    setTotal(updateTotal);
+    setPercentagePositive((good / updateTotal) * 100);
   };
   const handleBadClick = () => {
-    setBad(1 + bad);
+    const updateTotal = total + 1;
+    const updateTotalPro = totalpro - 1;
+    setBad(bad + 1);
+    setTotal(updateTotal);
+    setTotalpro(updateTotalPro);
+    setAverage(updateTotalPro / updateTotal);
+    setPercentagePositive((good / updateTotal) * 100);
   };
   return (
     <div>
@@ -37,6 +57,9 @@ const App = () => {
         <Content text={"good"} total={good} />
         <Content text={"neutral"} total={neutral} />
         <Content text={"bad"} total={bad} />
+        <Content text={"all"} total={total} />
+        <Content text={"average"} total={average} />
+        <Content text={"posetive"} total={percentagePositive} caracter={"%"} />
       </div>
     </div>
   );
