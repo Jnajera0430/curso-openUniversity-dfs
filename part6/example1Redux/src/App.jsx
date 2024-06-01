3349d0a8d76649379f26a904c5df9df0eb2a21dc
Jnajera0430 <desarrolador.sa.12@gmail.com>
2024-06-01 13:38:45 -0500

    ejemplo part6

--- part6/example1Redux/src/App.jsx
+++ part6/example1Redux/src/App.jsx
@@ -1,32 +1,4 @@
-import { createStore } from "redux";
-
-const noteReducer = (state = [], action) => {
-  if (action.type === "NEW_NOTE") {
-    return state.concat(action.payload);
-  }
-
-  return state;
-};
-
-const store = createStore(noteReducer);
-
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
-});
-
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
+import { store } from "./reducers/noteReducer";
 
 const App = () => {
   return (
